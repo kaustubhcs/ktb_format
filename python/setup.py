6465af0f3c311afe59c0105e5076bfa885e47c46
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2021-05-13 23:30:07 -0400

    TYPE FLAGS updated to include long

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Python and C++ ASCII to Binary Packer and Unpacker'
 LONG_DESCRIPTION = 'Packages ASCII data to Binary using KTB (Keep That Bit) format.'
 
